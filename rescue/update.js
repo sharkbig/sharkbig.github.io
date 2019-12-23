9752199a52bf1379af2d81c36ff2fa2c98b234d9
sharkbig <46228100+sharkbig@users.noreply.github.com>
2019-12-23 20:34:17 +0800

    finally! update and merge!

--- rescue/update.js
+++ rescue/update.js
@@ -1,16 +1,13 @@
-/// this is update
-
-
 var style_update = {
   'Point': new ol.style.Style({
     image: new ol.style.Circle({
-      // fill
+
       fill: new ol.style.Fill({
         color: 'rgba(255,255,0,0.4)'
       }),
+
       radius: 2,
 
-      // stroke
       stroke: new ol.style.Stroke({
         color: '#ff0',
         width: 1
@@ -20,22 +17,25 @@ var style_update = {
 
   'LineString': new ol.style.Style({
     stroke: new ol.style.Stroke({
-      color: '#002BFF',
-      width: 1.5
+      color: '#ff33d7',
+      width:1.5
     })
   }),
   'MultiLineString': new ol.style.Style({
     stroke: new ol.style.Stroke({
-      color: '#002BFF',
+      color: '#ff33d7',
       width: 1.5
     })
   })
 };
-var update = ol.layer.Vector({
+
+var update = new ol.layer.Vector({
 
   source: new ol.source.Vector({
-    url:'data/"20191223 陳芃+天人_B區神木群.gpx"',
+
+    url:'data/20191223陳芃+天人_B區神木群.gpx',
     format: new ol.format.GPX()
+
   }),
 
   style: function(feature){
@@ -43,9 +43,4 @@ var update = ol.layer.Vector({
   }
 });
 
-
-// plot on map
-var map = new ol.Map({
-    target: 'map',
-    layers: [update]
-  });
+map.addLayer(update)
