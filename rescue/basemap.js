063aa7b931b0e2eaaee7780eb76e0db1bd1e73b8
sharkbig <46228100+sharkbig@users.noreply.github.com>
2019-12-25 03:29:56 +0800

    Add basemap layer

--- rescue/basemap.js
+++ rescue/basemap.js
@@ -3,12 +3,50 @@
 var basemap = new ol.layer.Group({
     'title': 'Base maps',
     layers: [
+
+      //rudy's map
         new ol.layer.Tile({
-            title: '魯地圖',
-            source: new ol.source.XYZ({
-              url: 'https://rs.happyman.idv.tw/map/moi_osm/{z}/{x}/{y}.png'
-            })
-          }),
+          title: '魯地圖',
+          source: new ol.source.XYZ({
+            url: 'https://rs.happyman.idv.tw/map/moi_osm/{z}/{x}/{y}.png'
+          })
+        }),
+        // google satellite map
+        new ol.layer.Tile({
+          visible: false,
+          title: 'Google satellite',
+          source: new ol.source.XYZ({
+            url: 'http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}'
+          })
+        }),
+
+        // wmts nlcs
+
+
+        new ol.layer.Tile({
+          visible: false,
+          title: 'nlsc',
+          source: new ol.source.TileWMS({
+            url:'https://wms.nlsc.gov.tw/wms',
+            params:{
+              'LAYERS':'PHOTO_MIX',
+              'FORMAT':'image/png',
+              'crs':'EPSG:4326',
+              'dpiMode':'7',
+              'VERSION':'1.1.0'
+            },
+            transition:0,
+            serverType:'mapserver',
+
+          })
+        })
+
+
+
+
+
+
+
 
     ]
 })
--- rescue/track_group.js
+++ rescue/track_group.js
@@ -5,6 +5,7 @@ var track_group = new ol.layer.Group({
     layers: [
         new ol.layer.Vector({
             title: '1215-1222馬望海搜救路線',
+            visible:true,
             source: new ol.source.Vector({
               url: './data/1215-1222馬望海搜救路線._Lite.gpx',
               format: new ol.format.GPX()
@@ -14,28 +15,32 @@ var track_group = new ol.layer.Group({
               }
 
           }),
-          new ol.layer.Vector({
-            title: '20191223陳芃+天人_B區神木群',
-            source: new ol.source.Vector({
-              url: './data/20191223陳芃+天人_B區神木群.gpx',
-              format: new ol.format.GPX()
-            }),
-            style: function(feature) {
-                return style_update1[feature.getGeometry().getType()];
-              }
-            // projection: new ol.Projection("EPSG:4326")
+
+
+        new ol.layer.Vector({
+          title: '20191223陳芃+天人_B區神木群',
+          source: new ol.source.Vector({
+            url: './data/20191223陳芃+天人_B區神木群.gpx',
+            format: new ol.format.GPX()
           }),
-          new ol.layer.Vector({
-            title: '2019-12-24 tracks',
-            source: new ol.source.Vector({
-              url: './data/1224_lite.gpx',
-              format: new ol.format.GPX()
-            }),
-            style: function(feature) {
-                return style_1224[feature.getGeometry().getType()];
-              }
-            // projection: new ol.Projection("EPSG:4326")
-          })
+          style: function(feature) {
+              return style_update1[feature.getGeometry().getType()];
+            }
+          // projection: new ol.Projection("EPSG:4326")
+        }),
+
+
+        new ol.layer.Vector({
+          title: '2019-12-24 tracks',
+          source: new ol.source.Vector({
+            url: './data/1224_lite.gpx',
+            format: new ol.format.GPX()
+          }),
+          style: function(feature) {
+              return style_1224[feature.getGeometry().getType()];
+            }
+          // projection: new ol.Projection("EPSG:4326")
+        })
 
     ]
 })
