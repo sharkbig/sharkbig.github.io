f92eff539053c9b92ddaf84c9967bfa56b2a300f
typebrook <typebrook@gmail.com>
2019-12-30 18:44:28 +0800

    Add tracks for 邊吉岩

--- rescue/script/track_group.js
+++ rescue/script/track_group.js
@@ -130,7 +130,27 @@ var track_group = new ol.layer.Group({
         }
       }),
 
+      new ol.layer.Vector({
+        title: '2018_邊吉岩山西北陵探勘.kml',
+        source: new ol.source.Vector({
+          url:'2018_邊吉岩山西北陵探勘.kml',
+          format: new ol.format.KML()
+        }),
+        style: function(feature){
+          return style_setter(feature,'#DC00FF');
+        }
+      }),
 
+      new ol.layer.Vector({
+        title: '2018_鎮金邊.kml',
+        source: new ol.source.Vector({
+          url:'2018_鎮金邊.kml',
+          format: new ol.format.KML()
+        }),
+        style: function(feature){
+          return style_setter(feature,'#DC00FF');
+        }
+      })
     ]
 
 })
